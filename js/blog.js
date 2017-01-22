540ca9528e2ed3e86b8228083a623896948a6695
DGeero89 <dgeero89@gmail.com>
2017-01-21 18:50:42 -0800

    Finished init function.

--- js/blog.js
+++ js/blog.js
@@ -29,7 +29,7 @@
 		this.authImg = authImg || "http://placehold.it/48x48";
 		this.featureImg = featureImg || "http://placehold.it/896x370";
 		this.content = content || "";
-		this.tag = tags || [];
+		this.tags = tags || [];
 		this.comments = [];
 		
 	}
@@ -56,29 +56,75 @@
 					comments.push(comment);
 				}
 
+			},
+
+			__timeStamp: {
+
+				get: function() {
+					var oneDay = 1000 * 60 * 60 * 24,
+							datePosted = new Date(this.date),
+							today = new Date(Date.now()).getTime(),
+							differenceInMils = (today - datePosted);
+							daysAgo = Math.round(differenceInMils/oneDay);
+					
+					if(daysAgo > 1){
+						return daysAgo + " days ago";
+					} else if(daysAgo == 1){
+						return daysAgo + " day ago";
+					} else if(daysAgo <= -1) {
+						return Math.abs(daysAgo) + " days from now";
+					}
+				},
+
+				set: function(time) {
+					this.date = Date.parse(new Date(time));
+				}
+			
 			}
 
 		});
 
 
-
+	/*
+	*
+	* Init blog function used to text adding articles to blog object
+	*
+	*/
 
 	function blogInit() {
 		var mainContent = document.getElementById('main-content'),
-				articleTitle = mainContent.querySelector('.blog-title'),
-				authorImg = mainContent.querySelector('.author-img img').src;
+				articleTitle = mainContent.querySelector('.blog-title').textContent,
+				postDate = '1/19/2017',
+				author = mainContent.querySelector('.author').textContent,
+				authorImg = mainContent.querySelector('.author-img img').src,
+				featuredImg = mainContent.querySelector('.featured-img img').src,
+				articleContent = mainContent.querySelector('.blog-content').innerHTML,
+				articleTagElements = document.querySelectorAll('.tag-button'),
+				recentArticles = document.querySelectorAll('.article-title'),
+				tagsArray = [],
+				recentArticlesArr = [],
+				articlesArr = [];
 
-
-		var articles = [ 
-				new Article(articleTitle),
-				new Article("Getting Your Selected Articles Published on Encyclopedia")
-
-					];
-
-		for(var i = 0; i < articles.length; i++) {
-			blog.articles = articles[i];
+		// Push tags to tags array
+		for(var i = 0; i < articleTagElements.length; i++) {
+			tagsArray.push(articleTagElements[i].textContent);
 		}
 
+		// Push main article to articles array
+		articlesArr.push(new Article(articleTitle, postDate , author, authorImg, featuredImg, articleContent, tagsArray));
+
+		// Add recent articles to article array
+		for(var j = 0; j < recentArticles.length; j++){
+			articlesArr.push(new Article(recentArticles[j].textContent));
+		}
+
+		// Add article to blog article list
+		for(var k = 0; k < articlesArr.length; k++) {
+			blog.articles = articlesArr[k];
+		}
+
+		// Set Timestamp of post
+		mainContent.querySelector('.post-date').innerHTML = blog.articleList[0].__timeStamp;
 	}
 
 	blogInit();
