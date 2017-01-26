4a2f65e7343dbfcee444ff33580e3d0ac22ccb8f
Hannah Herbst <hherbst86@gmail.com>
2017-01-25 20:39:50 -0500

    added the jquery, updated css and html

---
+++ js/animate.js
@@ -0,0 +1,59 @@
+/**
+ * Created by hherb on 1/25/2017.
+ */
+
+
+$(document).ready(function(){
+    $("#load-more-link").click(function(){
+        var dots = $("#dots");
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "-=10px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "-=10px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "10"}, 0);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "+=2px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+        dots.animate({left: "-=10px"}, 175);
+        dots.animate({opacity: "0"}, 175);
+    });
+});
+
